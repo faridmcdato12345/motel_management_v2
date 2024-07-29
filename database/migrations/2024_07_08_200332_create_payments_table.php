853e27f5c39f738fff2050e57b0d3c5f9f1f4452
Farid Macadato <faridmcdato@gmail.com>
2024-07-30 04:32:08 +0800

    add payments page

--- database/migrations/2024_07_08_200332_create_payments_table.php
+++ database/migrations/2024_07_08_200332_create_payments_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('voucher_id')->constrained('vouchers')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->decimal('paid_amount', 10, 2);
-            $table->date('payment_date');
-            $table->enum('status',['Paid','Balance']);
+            $table->decimal('paid_amount', 10, 2)->nullable();
+            $table->date('payment_date')->nullable();
+            $table->enum('status',['Paid','Balance'])->nullable();
             $table->timestamps();
         });
     }
